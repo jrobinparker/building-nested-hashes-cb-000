8e82dae08f58e6092dc81f7c5a688d5da409bd80
Jeremy Parker <jrobinparker@gmail.com>
2018-06-12 20:32:27 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -23,14 +23,14 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {
-          name: "Benvolio",
-          age: "17",
-          attitude: "worried"
+          name: "Steven",
+          age: "30",
+          attitude: "confused"
         },
         {
-          name: "Mercutio",
-          age: "18",
-          attitude: "hot-headed"
+          name: "Nurse",
+          age: "44",
+          attitude: "worried"
         }
       ]
    }
